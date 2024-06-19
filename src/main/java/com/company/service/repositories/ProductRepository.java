71d2cfd8898fb48efc11679af787b9d023398990
Kauã Haymon <kauahaymon@hotmail.com>
2024-06-18 20:29:40 -0400

    Product

--- src/main/java/com/company/service/repositories/ProductRepository.java
+++ src/main/java/com/company/service/repositories/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.company.service.repositories;
 
-import com.company.service.entities.Category;
+import com.company.service.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
---
+++ src/main/java/com/company/service/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.company.service.services;
+
+import com.company.service.entities.Product;
+import com.company.service.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> product = repository.findById(id);
+        return product.orElse(null);
+    }
+}
