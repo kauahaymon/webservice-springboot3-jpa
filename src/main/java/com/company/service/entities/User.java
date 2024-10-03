29f6fc806c555abc0ec761dd34570e95dd444b1b
Kauã Haymon <kauahaymon@hotmail.com>
2024-10-03 15:08:21 -0400

    feat: add lombok to user

--- src/main/java/com/company/service/entities/User.java
+++ src/main/java/com/company/service/entities/User.java
@@ -2,7 +2,9 @@ package com.company.service.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import org.aspectj.weaver.ast.Or;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -10,6 +12,9 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
 @Table(name = "tb_user")
 public class User implements Serializable {
 
@@ -25,58 +30,12 @@ public class User implements Serializable {
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
-    public User() {}
-
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Order> getOrders() {
-        return orders;
     }
 
     @Override
