7863fa373a652d5f653959de4fb1d880fd1a4160
Kauã Haymon <kauahaymon@hotmail.com>
2024-10-03 15:31:12 -0400

    feat: add lombok to Order

--- src/main/java/com/company/service/entities/Order.java
+++ src/main/java/com/company/service/entities/Order.java
@@ -3,17 +3,20 @@ package com.company.service.entities;
 import com.company.service.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
@@ -23,7 +26,6 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GTM")
     private Instant moment;
-
     private Integer orderStatus;
 
     @ManyToOne
@@ -36,29 +38,11 @@ public class Order implements Serializable {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) // two entities will have same id
     private Payment payment;
 
-    public Order() {}
-
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         setOrderStatus(orderStatus);
         this.client = client;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Instant getMoment() {
-        return moment;
-    }
-
-    public void setMoment(Instant moment) {
-        this.moment = moment;
     }
 
     public OrderStatus getOrderStatus() {
@@ -67,26 +51,6 @@ public class Order implements Serializable {
 
     public void setOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus.getCode();
-    }
-
-    public User getClient() {
-        return client;
-    }
-
-    public void setClient(User client) {
-        this.client = client;
-    }
-
-    public Set<OrderItem> getItems() {
-        return items;
-    }
-
-    public Payment getPayment() {
-        return payment;
-    }
-
-    public void setPayment(Payment payment) {
-        this.payment = payment;
     }
 
     public Double getTotal() {
